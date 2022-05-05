45a94655a4c9a43cd67f04feb417f4fa7376034c
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2022-05-05 19:10:12 -0400

    add initial Spring program

---
+++ MusicSpringApp/src/main/java/com/example/MusicSpringApp/MusicSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSpringAppApplication.class, args);
+	}
+
+}
---
+++ MusicSpringApp/src/test/java/com/example/MusicSpringApp/MusicSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
