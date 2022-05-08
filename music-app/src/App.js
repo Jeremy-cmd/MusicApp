018418f57fd8043d07237b79b86ed5dfd21b9b12
Jeremy Gonzalez <jeremy.gonzalez1128@gmail.com>
2022-05-07 21:08:10 -0400

    add spotify auth

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import {Buffer} from 'buffer';
 
 function App() {
+  var client_id = process.env.REACT_APP_CLIENT_ID;
+  var client_secret = process.env.REACT_APP_CLIENT_SECRET;
+
+
+  useEffect(() => {
+
+    fetch('http://localhost:8081/music/getToken/' + client_id + '/' + client_secret, {
+       method: 'POST',
+       headers: new Headers({
+         'Authorization': 'Basic '+ (new Buffer(client_id + ':' + client_secret).toString('base64')),
+         'Content-Type': 'application/x-www-form-urlencoded'
+       }),
+       form: {
+         grant_type: 'client_credentials'
+       },
+       mode: 'cors',
+       json: true
+     }).then(response => response.json())
+        .then(res => console.log(res));
+
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
